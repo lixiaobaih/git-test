37ad2cd2ea321becab794c2641ee5663d77b5ed8
lxb <lxb@qq.com>
2022-10-14 17:59:22 +0800

    first commit

---
+++ src/main/java/com/xiaobai/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xiaobai.git;
+
+/**
+ * @author lyx
+ * @create 2022-10-14 17:51
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
